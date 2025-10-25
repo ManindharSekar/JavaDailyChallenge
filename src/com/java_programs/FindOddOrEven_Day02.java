8e2e7e8da50eec982802df1476a21f99fc2b5b02
Manindhar <manindharvlr@gmail.com>
2025-10-25 12:43:09 +0530

    Day 2

---
+++ src/com/java_programs/FindOddOrEven_Day02.java
@@ -0,0 +1,23 @@
+package com.java_programs;
+
+public class FindOddOrEven_Day02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=37;
+		switch(n%2){
+		case 0:
+			System.out.println("Given number "+n+" is even");
+			break;
+		case 1:
+			System.out.println("Given number "+n+" is odd");
+			break;
+		}
+
+	}
+
+}
