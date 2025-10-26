b96fe8d5ccbf428a6481074b568ba8f2a35f6581
Manindhar <manindharvlr@gmail.com>
2025-10-26 15:46:26 +0530

    Day 3

---
+++ src/com/java_programs/OddOrEvenNoModulAndDiv_Day03.java
@@ -0,0 +1,19 @@
+package com.java_programs;
+
+public class OddOrEvenNoModulAndDiv_Day03 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 78;
+		if ((n & 1) == 1) {
+			System.out.println("Given Number is Odd");
+		} else {
+			System.out.println("Given Number is Even");
+		}
+
+	}
+
+}
