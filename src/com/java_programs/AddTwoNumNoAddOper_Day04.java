d760eb73f90e643258f868f4324abef9b08ae8e1
Manindhar <manindharvlr@gmail.com>
2025-10-27 11:38:54 +0530

    Day 4

---
+++ src/com/java_programs/AddTwoNumNoAddOper_Day04.java
@@ -0,0 +1,17 @@
+package com.java_programs;
+
+public class AddTwoNumNoAddOper_Day04 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=6;
+		int b=25;
+		int c=a-(-b);
+		System.out.println("Sum of two number is: "+c);
+
+	}
+
+}
