3cdc3aade32f66b2ab4116509f9c33112f9aabcb
Manindhar <manindharvlr@gmail.com>
2025-10-29 11:11:52 +0530

    Day 6

---
+++ src/com/java_programs/CalculateSumOfNNum_Day06.java
@@ -0,0 +1,19 @@
+package com.java_programs;
+
+public class CalculateSumOfNNum_Day06 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 100;
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+		System.out.println("Sum Of N Number is: " + sum);
+
+	}
+
+}
--- src/com/java_programs/SumOfNumInArray_Day06.java
+++ src/com/java_programs/SumOfNumInArray_Day06.java
@@ -7,13 +7,13 @@ public class SumOfNumInArray_Day06 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int nums[]={2,5,4,7,1,4};
-		int sum=0;
-		for(int i=0;i<nums.length;i++){
-			sum=sum+nums[i];
+
+		int nums[] = { 2, 5, 4, 7, 1, 4 };
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum = sum + nums[i];
 		}
-		System.out.println("Sum of given number is: "+sum);
+		System.out.println("Sum of given number is: " + sum);
 
 	}
 
