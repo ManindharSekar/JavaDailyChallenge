5076edb1b47942560dde99f81eac9e8020ce8b9a
Manindhar <manindharvlr@gmail.com>
2025-11-05 18:17:27 +0530

    Day 13

---
+++ src/com/java_programs/Inheritance_Day13.java
@@ -0,0 +1,34 @@
+package com.java_programs;
+
+class A {
+	int a;
+
+	int getA() {
+		return a;
+	}
+}
+
+class B extends A {
+	int b;
+
+	int getB() {
+		return b;
+	}
+}
+
+public class Inheritance_Day13 {
+
+	// Program: INHERITANCE CONCEPT:
+	// Day: 13
+	// Added on: 5-Nov-2025
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		B b = new B();
+		b.a = 5;
+		b.b = 10;
+		System.out.println(b.getA());
+		System.out.println(b.getB());
+
+	}
+
+}
