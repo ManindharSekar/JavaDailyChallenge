7ae42c75cad25839ccfa4cf2e0d555007d707653
Manindhar <manindharvlr@gmail.com>
2025-11-08 15:49:01 +0530

    Day 16

---
+++ src/com/java_programs/ProducerConsumer_Day16.java
@@ -0,0 +1,88 @@
+package com.java_programs;
+
+class Q {
+	int n;
+	boolean valueSet = false;
+
+	synchronized int get() {
+		while (!valueSet) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println("Interrupted Exception caught");
+			}
+		}
+		System.out.println("Got:" + n);
+		valueSet = false;
+		notify();
+		return n;
+	}
+
+	synchronized void put(int n) {
+		while (valueSet) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println("Interrupted Exception caught");
+			}
+		}
+		this.n = n;
+		valueSet = true;
+		System.out.println("Put:" + n);
+		notify();
+	}
+}
+
+class Producer implements Runnable {
+	Q q;
+
+	Producer(Q q) {
+		this.q = q;
+		new Thread(this, "Producer").start();
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (true) {
+			q.put(i++);
+		}
+
+	}
+
+}
+
+class Consumer implements Runnable {
+	Q q;
+
+	Consumer(Q q) {
+		this.q = q;
+		new Thread(this, "Consumer").start();
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		while (true) {
+			q.get();
+		}
+
+	}
+
+}
+
+public class ProducerConsumer_Day16 {
+
+	// Program: IMPLEMENT PRODUCER CONSUMER DESIGN PATTERN IN JAVA USING WAIT,
+	// NOTIFY AND NOTIFY ALL
+	// Day: 16
+	// Added on: 8-Nov-2025
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Q q = new Q();
+		new Producer(q);
+		new Consumer(q);
+		System.out.println("Press Control-C to stop");
+
+	}
+
+}
