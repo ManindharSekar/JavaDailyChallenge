1db40de7afc2b104e2df780c8f0495694ba3835f
Manindhar <manindharvlr@gmail.com>
2025-11-13 15:35:22 +0530

    Day 20

---
+++ src/com/java_programs/ReverseArray_Day20.java
@@ -0,0 +1,30 @@
+package com.java_programs;
+
+public class ReverseArray_Day20 {
+
+	// Program: Reverse the given array
+	// Day: 20
+	// Added on: 13-Nov-2025
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = { 1, 2, 3, 4, 5, 6 };
+		for (int x : a) {
+			System.out.print(x + " ");
+		}
+		int i = 0;
+		int j = a.length - 1;
+		while (i < j) {
+			int temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i = i + 1;
+			j = j - 1;
+		}
+		System.out.println();
+		for (int x : a) {
+			System.out.print(x + " ");
+		}
+
+	}
+
+}
