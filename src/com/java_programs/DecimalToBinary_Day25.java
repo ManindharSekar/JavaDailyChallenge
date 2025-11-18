60f118ee9a065eb9ffdac100480d9db3a404a30e
Manindhar <manindharvlr@gmail.com>
2025-11-18 13:10:49 +0530

    Day 25

---
+++ src/com/java_programs/DecimalToBinary_Day25.java
@@ -0,0 +1,28 @@
+package com.java_programs;
+
+import java.util.Stack;
+
+public class DecimalToBinary_Day25 {
+
+	// Program: CONVERT DECIMAL TO BINARY USING STACK
+	// Day: 25
+	// Added on: 18-Nov-2025
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 7;
+		int r;
+		System.out.print("Binary Of " + n + " is ");
+		Stack stk = new Stack();
+		while (n != 0) {
+			r = n % 2;
+			stk.push(r);
+			n = n / 2;
+		}
+
+		while (!stk.isEmpty()) {
+			System.out.print(stk.pop());
+		}
+
+	}
+
+}
